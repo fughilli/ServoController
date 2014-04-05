ab42558def3962b64b59e4481d07e566bd4b2053
Kevin Balke <kbalke@ucla.edu>
2014-04-05 16:13:01 -0700

    Added license headers.

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
 /*
- * main.c
- *
- * AUTHOR: Kevin Balke
- *
- * (c) 2012-2014 Kevin Balke. All rights reserved.
- *
  * DESCRIPTION: I2C Servo Control Driver
  * I2C data to be presented in the form of 2 bytes per servo:
  *
@@ -27,7 +21,29 @@
  * Device Address: 0x39 (57 in decimal)
  *
  * Control signal output in the form of 8 channel PWM with feedback running at 50Hz per channel, 12 bit resolution (pulse widths offset)
+ *
+ *
+ * Copyright (C) 2012-2014  Kevin Balke
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
+
 #include <msp430.h>
 #include "simple_io.h"
 #include "simple_math.h"
